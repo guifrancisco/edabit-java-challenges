b92e4b7dd9e1baedc3e595602f7707f1ed3a78d6
guifrancisco <guifrancisco.almeida@outlook.com>
2024-01-16 23:50:34 -0300

    how many decimal places challenge

---
+++ Easy/HowManyDecimalPlaces.java
@@ -0,0 +1,14 @@
+// ##### CHALLENGE #####
+// https://edabit.com/challenge/kQZH2EDsn7LYvXk9S
+
+public class Challenge {
+	public static int getDecimalPlaces(String num) {
+		
+		if(!num.contains(".")){
+			return 0;
+		}
+		String[] aNum = num.split("\\.");
+		
+		return aNum[1].length();
+  }
+}
